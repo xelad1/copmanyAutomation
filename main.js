8fee6fb46a911806f873b99a5c891d3ef82be510
Alexander Dajani <Alex@Alexanders-MacBook-Air.local>
2015-12-21 13:32:55 -0800

    Added collections

--- main.js
+++ main.js
@@ -15,13 +15,17 @@ var layer = '';
 var company = '';
 
 //Collections
-var companyIDs = ['company_name', 'company_image_url', 'company_website_url',
-                  'company_description_field', 'company_twitter_username',
-                  'company_slug', 'company_canonical_url'];
+var companyIDs = ['#company_name', '#company_image_url', '#company_website_url',
+                  '#company_description_field', '#company_twitter_username',
+                  '#company_slug', '#company_canonical_url'];
 var companyVariables = [name, imageURL, websiteURL,
                         descriptionTitle, twitter, slug,
                         canonicalURL];
-var serviceIDs = [];
+var serviceIDs = ['#service_name', '#service_title', '#service_metatext',
+                  '#service_logo_url', '#service_website_url', '#service_documentation_url',
+                  '#service_twitter_username', '#service_image_url', '#service_description',
+                  '#service_press_url', '#service_features', '#service_pricing_url',
+                  '#service_slug', '#service_company_id_field > div > div > input', '#service_canonical_url'];
 var serviceVariables = [name, descriptionTitle, metatext,
                         imageURL, websiteURL, documentationURL,
                         twitter, imageURL, description, 
