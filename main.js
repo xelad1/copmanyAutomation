a8635dfc149727c905b23703f1f56452edf6c1e1
Alexander Dajani <Alex@Alexanders-Air.attlocal.net>
2015-12-20 23:23:51 -0800

    Added main variables

---
+++ main.js
@@ -0,0 +1,17 @@
+var name = "";
+var imageURL = "";
+var websiteURL = "";
+var descriptionTitle = "";
+var twitter = "";
+var slug = "";
+var canonicalURL = "";
+var metatext = "";
+var documentationURL = "";
+var description = "";
+var pressURL = "";
+var features = ""
+var pricingURL = "";
+var layer = "";
+var company = "";
+
+//
